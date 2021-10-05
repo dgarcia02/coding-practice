75839808dcaf66231dae00cb980a54406355ac06
Dulce Garcia <dulcegarcia@Dulces-Air.lan>
2021-10-04 22:14:08 -0500

    first javascript codewars example

---
+++ javascript/evenOrOdd.js
@@ -0,0 +1,10 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// function even_or_odd(number) {
+  
+// }
+
+const even_or_odd = (num) => {
+    console.log('example');
+}
+
+console.log(even_or_odd);
