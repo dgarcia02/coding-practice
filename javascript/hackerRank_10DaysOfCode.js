e395528049311b502c54d21f505c9e2d949a019b
Dulce Garcia <dulcegarcia@Dulces-Air.lan>
2021-10-07 19:56:52 -0500

    day 0 section 1 complete

---
+++ javascript/hackerRank_10DaysOfCode.js
@@ -0,0 +1,26 @@
+// Day 0 - JavaScript fundamentals
+
+// Section 1
+/**
+*   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
+*   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+*
+*	Parameter:
+*   parameterVariable - A string of text.
+**/
+// function greeting(parameterVariable) {
+//     // This line prints 'Hello, World!' to the console:
+//     console.log('Hello, World!');
+
+//     // Write a line of code that prints parameterVariable to stdout using console.log:
+// }
+
+const greeting = (parameterVariable) => {
+    parameterVariable = 'Welcome ro 10 days of JavaScript!'
+
+    console.log('Hello World!');
+    console.log(parameterVariable);
+}
+// console.log(greeting());
+
+// Section 2
