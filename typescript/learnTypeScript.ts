c16b98dea13525b472db134ebbe6708b51995e11
Dulce Garcia <dulcegarcia@Dulces-Air.lan>
2021-10-07 20:29:07 -0500

    learning typescript from codecademy

---
+++ typescript/learnTypeScript.ts
@@ -0,0 +1,16 @@
+// Why TypeScript was invented?
+// JavaScript is good for smaller applications, but TypeScript was invented by Microsoft for bigger applications and less bugs
+
+// Steps to begin a TypeScript file
+// 1. To create a TypeScript file use .js at the end
+// 2. Run the code through the transpiler to check if it can be read
+
+// Exercise 1.
+// In this exercise and throughout the lesson, we’ll provide you with a bash terminal to execute your code. We’ll be working with both TypeScript files (files with the extension .ts) and JavaScript files (files with the extension .js).
+
+// The TypeScript transcompiler can be used on the command line by running the tsc command:
+
+// tsc
+// This will create an equivalent .js file in the same directory as well as surface any errors found by the TypeScript transcompiler.
+
+// Run the TypeScript transcompiler using the tsc command.
